7b11eddf16030a6a4e5457a0138e0465226f9ebb
Max Franke <max.franke@acad.pucrs.br>
2020-11-20 02:01:37 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Fs = require('fs');
+const CsvReadableStream = require('csv-reader');
+
+let inputStream = Fs.createReadStream('DisTable.csv', 'utf8');
+
+inputStream
+    .pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true ,asObject:true}))
+    .on('data', function (row) {
+        console.log('A row arrived: ', row.writer)
+        transformToList(row.writer)
+    })
+    .on('end', function (data) {
+        console.log('No more rows!');
+    });
+
+const saveInMemory = () => {
+
+}
+
+const transformToList = (string) =>{
+    const re = /,\s/
+    let ar = string.split(re)
+    console.log(ar)
+    
+}
